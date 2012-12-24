373a48c4b0ab197de95effcc8b721e829f1c6864
bruin <bruin@fc13.(none)>
2012-12-24 13:37:17 +0800

    update

--- code/iconv_test.js
+++ code/iconv_test.js
@@ -7,8 +7,9 @@
 
 var iconv = require('iconv-lite');
 
-var s_heb = '\xe6\xe6';  // raw string in ISO 8859-8, aka hebrew
-var b_heb = new Buffer(s_heb, 'binary');  // new binary buffer from string
+//var s_heb = '\xe6\xe6';  // raw string in ISO 8859-8, aka hebrew
+//var b_heb = new Buffer(s_heb, 'binary');  // new binary buffer from string
+var b_heb = new Buffer([0xe6, 0xe6]);
 var s_ucs2 = iconv.decode(b_heb,'hebrew'); // decode heb buf to ucs2 string
 var b_utf8 = iconv.encode(s_ucs2, 'utf8'); // encode ucs2 string to utf8 buf
 var b_ucs2 = new Buffer(s_ucs2, 'ucs2');   // new ucs2 (utf16le) buffer from string
