b72665036020706a3b24504ef9eed600d1087439
xiongyw <xiongyw@hotmail.com>
2013-06-09 15:16:33 +0800

    add

---
+++ code/template.h
@@ -0,0 +1,17 @@
+#ifndef __TEMPLATE_H__
+#define __TEMPLATE_H__
+
+/*
+ * beautify by: indent -linux -l132 -nut -i4
+ */
+
+
+
+/* ----------------------------------------------- */
+/* blah blah blah                                  */
+/* ----------------------------------------------- */
+
+
+
+
+#endif /* __TEMPLATE_H__ */
