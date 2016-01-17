cd916d7a712cc8d4eed9a40fa3508a90665fe8e7
Yuwu Xiong <xiongyw@hotmail.com>
2016-01-17 05:22:42 -0500

    up

--- doc/it/linux/bin/zipall.sh
+++ doc/it/linux/bin/zipall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # zip the 1st level subdirectories under the current folder, for achiving stuff
+# note that if a directory contains space, this script does work properly...to be fixed.
 
 for d in $(ls -1Ap | grep /\$ | cut -d/ -f1); do
   zip -r $d.zip $d
