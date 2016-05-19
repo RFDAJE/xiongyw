37e2b41d5853c01ba5cc118bf9038d46a9b2f7ec
Yuwu Xiong <xiongyw@hotmail.com>
2016-05-19 16:31:53 +0800

    up

--- code/js/jsgo/jsgo.js
+++ code/js/jsgo/jsgo.js
@@ -3,16 +3,16 @@ var JSGO = (function(){
     /*##############################################################
       # constant definitions
       #############################################################*/
-    var MAX_GRID =  19;          /* maximum board size, inclusive */
-    var MIN_GRID =  2;           /* minimum board size, inclusive */
-    var PASS = (MAX_GRID + 1)    /* (PASS, PASS) is a pass move */
-    var MAX_HANDICAPS = 9
-    var MIN_HANDICAPS = 2
+    const MAX_GRID =  19;          /* maximum board size, inclusive */
+    const MIN_GRID =  2;           /* minimum board size, inclusive */
+    const PASS = (MAX_GRID + 1);   /* (PASS, PASS) is a pass move */
+    const MAX_HANDICAPS = 9;
+    const MIN_HANDICAPS = 2;
 
     /*##############################################################
       # enum
       #############################################################*/
-    var PLAYER = { 
+    const PLAYER = { 
         WHITE: "W",
         BLACK: "B",
         NONE:  "N"  // used to indicate KO status when there is no KO
@@ -22,7 +22,7 @@ var JSGO = (function(){
      * vertex color (state): restrict the values to be 0,1,2,3, 
      * i.e, can be stored in two bits.
      */
-    var COLOR = {
+    const COLOR = {
     	WHITE: "W",  /* occupied by a white stone */
     	BLACK: "B",  /* occupied by a white stone */
     	GREEN: "G",  /* not occupied and valid for the next mover */
@@ -30,7 +30,7 @@ var JSGO = (function(){
     };
 
     /* types of rules */
-    var RULE = {
+    const RULE = {
     	CHINESE: "CHI",
     	JAPANESE: "JAP",
     	INGS: "ING",
@@ -38,7 +38,7 @@ var JSGO = (function(){
     };
 
     /* validity of a move */
-    var MOVE = {
+    const MOVE = {
     	GOOD: 0,   /* move is valid */
     	PILE: 1,   /* invalid (vertex already occupied) */
     	INKO: 2,   /* invalid (vertex in ko_t) */
@@ -88,7 +88,7 @@ var JSGO = (function(){
      */
 
     /* an array of vertices 
-     *  var vetices = {
+     *  var vertices = {
      *      "len": 10,
      *      "rows": [1,2,3,4,5,6,7,8,9,10],
      *      "cols": [1,2,3,4,5,6,7,8,9,10]};
@@ -163,12 +163,12 @@ var JSGO = (function(){
 
     	/* sanity check */
     	if(nrow > MAX_GRID || ncol > MAX_GRID || nrow < MIN_GRID || ncol < MIN_GRID) {
-            console.log("illegal board size!");
+            console.log("ERROR: illegal board size!");
     		return null;
         }
 
         if (rule != RULE.CHINESE && rule != RULE.JAPANESE && rule != INGS && rule != RULE.BRITISH) {
-            console.log("illegal rule!");
+            console.log("ERROR: illegal rule!");
             return null;
         }
 
@@ -197,6 +197,175 @@ var JSGO = (function(){
         return JSON.parse(JSON.stringify(mstate));
     }
 
+    function mstate_get_vertex_color(mstate, row, col){
+        // check range
+        if (row >= mstate.nrow || col >= mstate.ncol || row < 0 || col < 0) {
+            console.log("ERROR: row/col out of range!");
+            return null;
+        }
+        return mstate.color[row][col];
+    }
+
+    const DIRECT = 0;          /* direct called */
+    const NORTH  = 1;          /* northern search */
+    const EAST   = 2;          /* eastern search */
+    const SOUTH  = 4;          /* southern search */
+    const WEST   = 8;          /* western search */
+
+    /*
+     * It recursively finds out all the pieces of a string has vertex 
+     * (row, col), returns the nr of pieces in the string.
+     *
+     * "dir" tells the recursive search direction of the caller, take
+     * one of DIRECT/NORTH/EAST/SOUTH/WEST, where DIRECT means it's a
+     * direct call from outside, not recursively called by itself.
+     */
+    function s_get_string(state, row, col, vertices, dir){
+
+    	var who;  /* occupant of (row, col), either BLACK or WHITE */ 
+    	var i;
+
+    	if(dir == DIRECT){  /* called from outside, not recursive call */
+    		vertices.len = 0;
+        }
+
+    	/* check if (row, col) already in vertices */
+    	for(i = 0; i < vertices.len; i ++){
+    		if(vertices.rows[i] == row && vertices.cols[i] == col) {
+    			return 0;    /* return value ignored */
+              }
+    	}
+
+    	/* add the current piece in the string list first */
+    	vertices.rows.push(row);
+    	vertices.cols.push(col);
+    	vertices.len ++;
+
+    	who = mstate_get_vertex_color(state, row, col);
+
+    	/*
+    	 * recursively search in N/E/S/W directions 
+    	 */
+    	 
+    	/* northern search */
+    	if(!(dir & SOUTH) && row > 0 && mstate_get_vertex_color(state, row - 1, col) == who)
+    		s_get_string(state, row - 1, col, vertices, NORTH);
+
+    	/* eastern search */
+    	if(!(dir & WEST) && col < state.ncol - 1 && mstate_get_vertex_color(state, row, col + 1) == who)
+    		s_get_string(state, row, col + 1, vertices, EAST);
+
+    	/* southern search */
+    	if(!(dir & NORTH) && row < state.nrow - 1 && mstate_get_vertex_color(state, row + 1, col) == who)
+    		s_get_string(state, row + 1, col, vertices, SOUTH);
+
+    	/* western search */
+    	if(!(dir & EAST) && col > 0 && mstate_get_vertex_color(state, row, col - 1) == who)
+    		s_get_string(state, row, col - 1, vertices, WEST);
+
+    	if(dir != DIRECT){
+    		/* recursive called, ignored return value */ 
+    		return 0; 
+    	}
+    	else{
+    		/* called from outside, return string size */
+    		return vertices.len;
+    	}
+    }
+
+    /*
+     * It checks whether the vertex (row, col) is a Degree Of Freedom.
+     * If it is and not in "vdof" yet, add (row, col) to "vdof".
+     *
+     * RETURN VALUE
+     * 0:  normal terminate
+     * - 1:  (row, col) not empty, simply return
+     */
+    function s_check_dof(state, row, col, dof) {
+
+    	var i, found;
+
+    	/* dof means it should be unoccupied */
+    	if(mstate_get_vertex_color(state, row, col) == COLOR.BLACK ||
+    	   mstate_get_vertex_color(state, row, col) == COLOR.WHITE) {
+    		return   - 1;
+        }
+
+    	/* check if (row, col) already in "dof" */
+    	found = false; /* assume not in "dof" first */
+    	for(i = 0; i < dof.len; i ++){
+    		if(dof.cols[i] == col && dof.rows[i] == row){
+    			found = true;
+    			break;
+    		}
+    	}
+
+    	if(!found){  /* add (row, col) to "dof" */
+    		dof.rows.push(row);
+    		dof.cols.push(col);
+    		dof.len ++;
+    	}
+
+    	return 0;
+    }
+
+
+    /*
+     * finds out all the pieces of a string has vertex at (row, col), 
+     * and store the pieces' locatioins supplied "vlist". It's 
+     * guaranteed that "vlist" has been allocated enought space by 
+     * the caller.
+     *
+     * RETURN: the string and its dof
+     */
+    function mstate_get_string(state, row, col){
+
+    	/* range check */
+    	if(row >= state.nrow || col >= state.ncol || row < 0 || col < 0) {
+            console.log("ERROR: out of range!");
+    		return null;
+        }
+
+    	/* sanity check: it should not be empty */
+    	if(mstate_get_vertex_color(state, row, col) != COLOR.BLACK &&
+    	   mstate_get_vertex_color(state, row, col) != COLOR.WHITE) {
+            console.log("ERROR: the vertices is not occupied by any player yet!");
+    		return null;
+        }
+
+        var string = {"len": 0, "rows": [], "cols": []};
+        var dof = {"len": 0, "rows": [], "cols": []};
+
+        /* get the string first */
+    	var size = s_get_string(state, row, col, vertices, DIRECT);
+
+        /* get the dof of the string */
+    	for (var i = 0; i < size; i ++) {
+
+    		var ro, co;
+    		ro = string.rows[i];
+    		co = string.cols[i];
+
+    		/* southern check */
+    		if(ro < state.nrow - 1)  
+    			s_check_dof(state, ro + 1, co, dof);
+
+    		/* eastern check */
+    		if(co < state.ncol - 1)  
+    			s_check_dof(state, ro, co + 1, dof);
+
+    		/* northern check */
+    		if(ro > 0)                
+    			s_check_dof(state, ro - 1, co, dof);
+
+    		/* western check */
+    		if(co > 0)                
+    			s_check_dof(state, ro, co - 1, dof);
+    	}
+
+        /* return the string and its dof */
+        return {"vertices": vertices, "dof": dof};
+    }
 
 
     /*
@@ -220,6 +389,8 @@ var JSGO = (function(){
         // methods:
         "mstate_create": mstate_create,
         "mstate_clone": mstate_clone,
+        "mstate_get_vertex_color": mstate_get_vertex_color,
+        "mstate_get_string": mstate_get_string,
     };
 
 }());
