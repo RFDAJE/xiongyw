ec03df737b897f72730c66acbadaecea7b742272
Yuwu Xiong <xiongyw@hotmail.com>
2016-05-20 12:50:04 +0800

    up

--- code/js/jsgo/jsgo2canvas.js
+++ code/js/jsgo/jsgo2canvas.js
@@ -2,14 +2,10 @@
 
 var JG2C = (function(){
 
-    var g_rows = 19;
-    var g_cols = 19;
-    var g_grid_size = 45; // in pixel
-    var g_stone_radius = (g_grid_size - 3) / 2;
-    var g_canvas_width = (g_cols + 3) * g_grid_size; 
-    var g_canvas_height = (g_rows + 3) * g_grid_size; 
-    var g_line_width_1 = 1;  // inner lines
-    var g_line_width_2 = 2;  // outter lines
+    const GRID_SIZE = 45; // in pixel
+    const STONE_RADIUS = (GRID_SIZE - 3) / 2;
+    const LINE_WIDTH_1 = 1;  // inner lines
+    const LINE_WIDTH_2 = 2;  // outter lines
 
     var _canvas = null;
     var _ctx = null;
@@ -17,26 +13,37 @@ var JG2C = (function(){
      
     function set_canvas(c) {
         _canvas = c;
-	    _canvas.width = g_canvas_width;
-    	_canvas.height = g_canvas_height;
 	    _ctx = _canvas.getContext('2d');
     }
 
     function set_mstate(m) {
         _mstate = m;
+	    _canvas.width = (_mstate.ncol + 3) * GRID_SIZE;
+    	_canvas.height = (_mstate.nrow + 3) * GRID_SIZE;;
+    }
+
+    function get_mstate() {
+        return _mstate;
+    }
+
+    function get_canvas_size() {
+        if (_mstate === null)
+            return null;
+        
+        return {"width": _canvas.width, "height": _canvas.height };
     }
     
     function circle_at(row, col, radius, color) {
         // find out the center
-        var x = g_grid_size * (col + 2);
-        var y = g_grid_size * (row + 2);
+        var x = GRID_SIZE * (col + 2);
+        var y = GRID_SIZE * (row + 2);
 
         
         _ctx.beginPath();
         _ctx.arc(x, y, radius, 0, Math.PI * 2, false);
         
         _ctx.fillStyle = color;
-        _ctx.lineWidth = g_line_width_1;
+        _ctx.lineWidth = LINE_WIDTH_1;
         _ctx.fill();
         _ctx.closePath();
         _ctx.stroke();
@@ -44,8 +51,8 @@ var JG2C = (function(){
 
     function text_at(row, col, text, font, textAlign, textBaseline) {
         // find out the center
-        var x = g_grid_size * (col + 2);
-        var y = g_grid_size * (row + 2);
+        var x = GRID_SIZE * (col + 2);
+        var y = GRID_SIZE * (row + 2);
         _ctx.font = font;
         _ctx.textAlign = textAlign;
         _ctx.textBaseline = textBaseline;
@@ -59,25 +66,25 @@ var JG2C = (function(){
         _ctx.strokeStyle = "black";
         _ctx.fillStyle="#EEB422";
         _ctx.beginPath();
-        _ctx.lineWidth = g_line_width_2;
-        _ctx.fillRect(g_grid_size, g_grid_size, g_grid_size * (mstate.ncol + 1), g_grid_size * (mstate.nrow + 1));
+        _ctx.lineWidth = LINE_WIDTH_2;
+        _ctx.fillRect(GRID_SIZE, GRID_SIZE, GRID_SIZE * (mstate.ncol + 1), GRID_SIZE * (mstate.nrow + 1));
         _ctx.stroke();
         
         // draw board grid
         _ctx.beginPath();
-        _ctx.lineWidth = g_line_width_1;
+        _ctx.lineWidth = LINE_WIDTH_1;
         for (i = 0; i < mstate.nrow; i ++) {
-            x1 = g_grid_size * 2;
-            x2 = (mstate.ncol + 1) * g_grid_size;
-            y1 = (i + 2) * g_grid_size;
+            x1 = GRID_SIZE * 2;
+            x2 = (mstate.ncol + 1) * GRID_SIZE;
+            y1 = (i + 2) * GRID_SIZE;
             _ctx.moveTo(x1, y1);
             _ctx.lineTo(x2, y1);
         }
         
         for (i = 0; i < mstate.ncol; i ++) {
-            y1 = g_grid_size * 2;
-            y2 = (mstate.nrow + 1) * g_grid_size;
-            x1 = (i + 2) * g_grid_size;
+            y1 = GRID_SIZE * 2;
+            y2 = (mstate.nrow + 1) * GRID_SIZE;
+            x1 = (i + 2) * GRID_SIZE;
             _ctx.moveTo(x1, y1);
             _ctx.lineTo(x1, y2);
         }
@@ -99,26 +106,109 @@ var JG2C = (function(){
         // draw grid marks: AB... & 1234
         var mark_font = "16pt sans-serif";
         _ctx.fillStyle = "black";
-        for (i = 0; i < mstate.nrow; i ++) {
-            text_at(i, - 1, (i + 1).toString()+ " ", mark_font, "end", "middle");
-            text_at(i, mstate.ncol, " " + (i + 1).toString()+ " ", mark_font, "start", "middle");
+        for (i = mstate.nrow; i > 0; i --) { // 1 starts from the bottom of the board
+            text_at(mstate.nrow - i, - 1, i.toString()+ " ", mark_font, "end", "middle");
+            text_at(mstate.nrow - i, mstate.ncol, " " + i.toString()+ " ", mark_font, "start", "middle");
         }
 
-        for (i = 0; i < mstate.ncol; i ++) {
-            text_at(- 1, i, "ABCDEFGHIJLMNOPQRST"[i], mark_font, "center", "bottom");
-            text_at(mstate.nrow, i, "ABCDEFGHIJLMNOPQRST"[i], mark_font, "center", "top");
+        for (i = 0; i < mstate.ncol; i ++) {  // not using 'I'
+            text_at(- 1, i, "ABCDEFGHJKLMNOPQRST"[i], mark_font, "center", "bottom");
+            text_at(mstate.nrow, i, "ABCDEFGHJKLMNOPQRST"[i], mark_font, "center", "top");
         }
         
-        //circle_at(0, 0, g_stone_radius, "white");
-        //circle_at(0, 1, g_stone_radius, "black");
+        //circle_at(0, 0, STONE_RADIUS, "white");
+        //circle_at(0, 1, STONE_RADIUS, "black");
 
     }
+
+
+    /*
+     *----------------------------------
+     * event handlers
+     *----------------------------------
+     */
+    function onClick(e) {
+        console.log("onClick: x=" + e.clientX + ", y=" + e.clientY + ", detail=" + e.detail);
+    }
+
+    function onDoubleClick(e) {
+        console.log("onDoubleClick: x=" + e.clientX + ", y=" + e.clientY + ", detail=" + e.detail);
+    }
+
+    function onRightClick(e) {
+        console.log("onRightClick: x=" + e.clientX + ", y=" + e.clientY + ", detail=" + e.detail);
+    }
     
+    //
+    // the fact: canvas fires onmousemove events even the mouse position is not moved.
+    // the impact: when click event happens, the current position is added into the path, but
+    //   a mouse move event with the same position will be emitted. if we want to draw the
+    //   current path, the last two points are identical...then the Bezier curve will not be draw,
+    //   probably because the linear equations are not properly formed...
+    // the remedy: omit mouse move event when the position is not changed. use closure the keep the 
+    //   previous positions;
+    var onMouseMove = (function() {
+        // previous e.clientX and e.clientY
+        var pre_x = -1;
+        var pre_y = -1;
+        return function(e){
+            if(pre_x === e.clientX && pre_y === e.clientY) {
+                // console.log("move without position change!");
+                return;
+            }
+            pre_x = e.clientX;
+            pre_y = e.clientY;
+            console.log("onMouseMove: x=" + e.clientX + ", y=" + e.clientY + ", detail=" + e.detail);
+            }
+    }());
+
+    function onMouseWheel(e) {
+        console.log("onMouseWheel: dx=" + e.deltaX + ", dy=" + e.deltaY);
+    }
+
+    function onMouseLeave(e) {
+        console.log("onMouseLeave: e=" + e);
+    }
+
+    function onMouseEnter(e) {
+        console.log("onMouseEnter: e=" + e);
+    }
+
+    function handleFocus(e) {
+        if(e.type == 'mouseover'){
+            _canvas.focus();
+            return false;
+        }else if(e.type == 'mouseout'){
+            _canvas.blur();
+            return false;
+        }
+        return true;
+    };
+
+    function handleKeyDown(e) {
+        console.log("key down:" + e.keyCode);
+        return false;
+    };
+
+
 
     return {
         "set_canvas": set_canvas,
         "set_mstate": set_mstate,
-        "draw_mstate": draw_mstate
+        "get_mstate": get_mstate,
+        "get_canvas_size": get_canvas_size,
+        "draw_mstate": draw_mstate,
+
+        // event handlers
+        "onMouseMove": onMouseMove,
+        "onClick": onClick,
+        "onDoubleClick": onDoubleClick,
+        "onRightClick": onRightClick,
+        "onMouseWheel": onMouseWheel,
+        "onMouseEnter": onMouseEnter,
+        "onMouseLeave": onMouseLeave,
+        "handleFocus": handleFocus,
+        "handleKeyDown": handleKeyDown
     };
 }());
 
